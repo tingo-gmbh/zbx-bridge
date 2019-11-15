a5cb2e361a28a771bdb645a709c1a602077b098a
Nicolas Bruegger <brueggern@gmail.com>
2019-11-15 11:48:59 +0100

    src/server.ts

--- src/server.ts
+++ src/server.ts
@@ -8,19 +8,20 @@ const app = express();
 // Load dot env file
 config({ path: resolve(__dirname, "./../.env") });
 
+// Route definitions
 app.get("/", (req, res) => {
-    if (process.env[req.query.command]) {
-        const [s, k] = process.env[req.query.command].split(',');
-        const zabbixSenderCommand = `/usr/local/bin/zabbix_sender -z ${address()} -s "${s}" -k "${k}" -o "${req.query.command}" -vv`;
-        const stdout = execSync(zabbixSenderCommand);
-        res.send(`Output: ${stdout}`);
-    } else {
-        res.send(`Command not found!`);
-    }
-})
+  if (process.env[req.query.command]) {
+    const [s, k] = process.env[req.query.command].split(",");
+    const zabbixSenderCommand = `/usr/local/bin/zabbix_sender -z ${address()} -s "${s}" -k "${k}" -o "${req.query.command}" -vv`;
+    const stdout = execSync(zabbixSenderCommand);
+    res.send(`Output: ${stdout}`);
+  } else {
+    res.send(`Command not found!`);
+  }
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-     console.log(`Server is running in http://localhost:${PORT}`)
-})
+  console.log(`Server is running in http://localhost:${PORT}`);
+});
