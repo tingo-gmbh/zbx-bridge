d8b3a5e75a4742e3ba7f5ab4ef97d0df195941f8
Nicolas Bruegger <brueggern@gmail.com>
2020-02-17 10:17:45 +0100

    Typo

--- src/server.ts
+++ src/server.ts
@@ -30,5 +30,5 @@ app.get("/", (req, res) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running in http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
